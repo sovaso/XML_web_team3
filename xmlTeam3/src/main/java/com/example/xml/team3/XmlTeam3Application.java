950dc1955708db076c7d1328953be7a84826ff86
Marina Vojnovic <marina.vojnovic1997@gmail.com>
2020-02-01 12:08:11 +0100

    initial backend commit with schemas

---
+++ xmlTeam3/src/main/java/com/example/xml/team3/XmlTeam3Application.java
@@ -0,0 +1,13 @@
+package com.example.xml.team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlTeam3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlTeam3Application.class, args);
+	}
+
+}
---
+++ xmlTeam3/src/test/java/com/example/xml/team3/XmlTeam3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xml.team3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlTeam3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
