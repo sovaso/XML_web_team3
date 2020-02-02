87d75bdc011934f2c071522bc3e5b0b99cc7b883
LeonaNedeljkovic <leona.nedeljkovic@gmail.com>
2020-02-02 18:27:47 +0100

    added for adding sw

--- xmlFrontend/src/app/scientific-works/scientific-works.component.ts
+++ xmlFrontend/src/app/scientific-works/scientific-works.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractDto} from 'src/app/dto/Abstract.dto'
+import {AuthorDto} from 'src/app/dto/Author.dto'
+import {HeaderDto} from 'src/app/dto/Header.dto'
+import {ReferenceDto} from 'src/app/dto/Reference.dto'
+import {ScientificWorkDto} from 'src/app/dto/ScientificWork.dto'
 
 @Component({
   selector: 'app-scientific-works',
@@ -8,26 +13,41 @@ import { Component, OnInit } from '@angular/core';
 export class ScientificWorksComponent implements OnInit {
 
   title: string='';
-  authors: AuthorDTO[]=[];
-  abstract : AbstractDTO;
+  authors: AuthorDto[]=[];
+  abstract : AbstractDto;
   paragpraphs: string='';
   reference: string[]=[]
   numOfAuthors: number=0;
   numOfKeywords: number=0;
   numOfParagraph: number=0;
   numOfReferences: number=0;
-  allScientificWorks: ScientificWorkDTO[]=[]
+  allPublished: ScientificWorkDto[]=[]
+  coverLetter: string='';
 
   constructor() { }
 
   ngOnInit() {
     this.abstract={
-      id:'',
-      name:'',
-      address:'',
-      description:'',
-      locationZone:[]
+      purpose:'',
+      design:'',
+      findings:'',
+      limitations:'',
+      originality: '',
+      type: '',
+      keywords: []
     }
+  }
+
+  addParagraph(){
+
+  }
+
+  addKeyword(){
+    
+  }
+
+  addAuthor(){
+    
   }
 
 }
