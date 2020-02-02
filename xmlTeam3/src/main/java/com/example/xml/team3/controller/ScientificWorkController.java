141a235798d6d6ebf48569d7b1c17f5f2f84c506
Vasilije Pantic <vpantic10@gmail.com>
2020-02-02 19:22:53 +0100

    fixed model for scientificWork

--- xmlTeam3/src/main/java/com/example/xml/team3/controller/ScientificWorkController.java
+++ xmlTeam3/src/main/java/com/example/xml/team3/controller/ScientificWorkController.java
@@ -1,5 +1,38 @@
 package com.example.xml.team3.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.xml.team3.dto.ScientificWorkDTO;
+import com.example.xml.team3.model.scientificwork.Paragraph;
+import com.example.xml.team3.model.scientificwork.ScientificWork;
+import com.example.xml.team3.service.ScientificWorkService;
+
+@RestController
+@RequestMapping(value = "/scientificWork")
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 public class ScientificWorkController {
 
+	@Autowired
+	ScientificWorkService scientificWorkService;
+	
+	@PostMapping(value = "/create")
+	public ResponseEntity<?> createScientificWork(@RequestBody @Valid ScientificWorkDTO scientificWorkDTO) {
+		ScientificWork retVal = new ScientificWork();
+		retVal.setTitle(scientificWorkDTO.getTitle());
+		//paragraf
+		for (String s : scientificWorkDTO.getParagraphs()) {
+			Paragraph p = new Paragraph();
+			//p.getContent()
+		}
+		retVal.getComment().addAll(scientificWorkDTO.getComments());
+		return null;
+	}
 }
