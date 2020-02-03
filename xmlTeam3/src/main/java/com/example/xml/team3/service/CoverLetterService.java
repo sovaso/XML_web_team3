c0729da2c95d1904cb49a95a2bf3cf496740a98e
Vasilije Pantic <vpantic10@gmail.com>
2020-02-03 14:53:59 +0100

    added cover letter controller

--- xmlTeam3/src/main/java/com/example/xml/team3/service/CoverLetterService.java
+++ xmlTeam3/src/main/java/com/example/xml/team3/service/CoverLetterService.java
@@ -11,4 +11,6 @@ public interface CoverLetterService {
 	public String updateCoverLetter(String id, CoverLetter coverLetter) throws Exception;
 
 	public boolean deleteCoverLetter(String id) throws Exception;
+	
+	public CoverLetter findByScientificWorkId(String id) throws Exception;
 }
